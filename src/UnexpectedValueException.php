93dd63b85da2f6bf2c6d4d1888148a934edcceae
Thomas Nunninger <thomas.nunninger@scaleupventures.com>
2019-02-18 03:20:56 +0100

    DataTypeMetadata copied from other project.

---
+++ src/UnexpectedValueException.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * This file is part of ScaleUpStack/EasyObject.
+ *
+ * For the full copyright and license information, please view the README.md and LICENSE.md files that were distributed
+ * with this source code.
+ *
+ * @copyright 2019 - present ScaleUpVentures GmbH, https://www.scaleupventures.com
+ * @link      https://github.com/scaleupstack/easy-object
+ */
+
+namespace ScaleUpStack\EasyObject;
+
+class UnexpectedValueException extends \UnexpectedValueException
+{
+}
