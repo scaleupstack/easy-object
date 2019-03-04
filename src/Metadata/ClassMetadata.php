4e2153abe7ace004b6eccb1ab0e43a9c7fd6dd02
Thomas Nunninger <thomas.nunninger@scaleupventures.com>
2019-03-04 19:33:20 +0100

    ClassMetadata: Added virtual property and method metadata.

--- src/Metadata/ClassMetadata.php
+++ src/Metadata/ClassMetadata.php
@@ -12,19 +12,40 @@
 
 namespace ScaleUpStack\EasyObject\Metadata;
 
+use ScaleUpStack\Annotations\Annotation\MethodAnnotation;
+use ScaleUpStack\Annotations\Annotation\PropertyReadAnnotation;
 use ScaleUpStack\Annotations\Annotations;
 
 class ClassMetadata extends \Metadata\ClassMetadata
 {
-    /**
-     * @var array
-     */
     public $annotations;
 
-    /**
-     * @return string
-     */
-    public function serialize()
+    public $virtualPropertyMetadata = [];
+
+    public $virtualMethodMetadata = [];
+
+    public function __construct(string $name, Annotations $annotations)
+    {
+        parent::__construct($name);
+        $this->setAnnotations($annotations);
+    }
+
+    private function setAnnotations(Annotations $annotations)
+    {
+        $this->annotations = $annotations;
+
+        /** @var PropertyReadAnnotation $readPropertyAnnotation */
+        foreach ($annotations->annotationsByTag('property-read') as $readPropertyAnnotation) {
+            $this->virtualPropertyMetadata[$readPropertyAnnotation->propertyName()] = $readPropertyAnnotation;
+        }
+
+        /** @var MethodAnnotation $methodAnnotation */
+        foreach ($annotations->annotationsByTag('method') as $methodAnnotation) {
+            $this->virtualMethodMetadata[$methodAnnotation->methodName()] = $methodAnnotation;
+        }
+    }
+
+    public function serialize() : string
     {
         return serialize(
             [
@@ -41,9 +62,11 @@ class ClassMetadata extends \Metadata\ClassMetadata
     {
         list(
             $parent,
-            $this->annotations
+            $annotations
         ) = unserialize($str);
 
         parent::unserialize($parent);
+
+        $this->setAnnotations($annotations);
     }
 }
--- tests/PhpUnit/Metadata/ClassMetadataTest.php
+++ tests/PhpUnit/Metadata/ClassMetadataTest.php
@@ -12,7 +12,11 @@
 
 namespace ScaleUpStack\EasyObject\Tests\PhpUnit\Metadata;
 
+use ScaleUpStack\Annotations\Annotation\MethodAnnotation;
+use ScaleUpStack\Annotations\Annotation\PropertyReadAnnotation;
+use ScaleUpStack\Annotations\Annotations;
 use ScaleUpStack\EasyObject\Metadata\ClassMetadata;
+use ScaleUpStack\EasyObject\Tests\Resources\Metadata\FromFileReaderTestClass;
 use ScaleUpStack\EasyObject\Tests\Resources\TestCase;
 
 /**
@@ -22,13 +26,50 @@ final class ClassMetadataTest extends TestCase
 {
     /**
      * @test
+     * @covers ::__construct()
+     * @covers ::setAnnotations()
+     */
+    public function it_stores_metadata_for_virtual_properties_and_methods()
+    {
+        // given a class name, and some Annotations with a PropertyReadAnnotation and a MethodAnnotation
+        $className = FromFileReaderTestClass::class;
+        $annotations = new Annotations();
+        $annotations->add('property-read', 'string $someProperty', Annotations::CONTEXT_CLASS);
+        $annotations->add('method', 'int getSomeProperty()', Annotations::CONTEXT_CLASS);
+
+        // when creating the ClassMetadata
+        $classMetadata = new ClassMetadata($className, $annotations);
+
+        // then the annotations are stored in the ClassMetadata
+        // and the virtual properties and methods are available directly
+        $this->assertSame($annotations, $classMetadata->annotations);
+        $this->assertEquals(
+            [
+                'someProperty' => new PropertyReadAnnotation('property-read', 'string $someProperty'),
+            ],
+            $classMetadata->virtualPropertyMetadata
+        );
+
+        $this->assertEquals(
+            [
+                'getSomeProperty' => new MethodAnnotation('method', 'int getSomeProperty()'),
+            ],
+            $classMetadata->virtualMethodMetadata
+        );
+    }
+
+    /**
+     * @test
      * @covers ::serialize()
      * @covers ::unserialize()
      */
     public function it_can_be_serialized_and_unserialized()
     {
-        // given a ClassMetadata instance
-        $metadata = new ClassMetadata(ClassMetadata::class);
+        // given a ClassMetadata instance with virtual properties and methods
+        $annotations = new Annotations();
+        $annotations->add('property-read', 'string $someProperty', Annotations::CONTEXT_CLASS);
+        $annotations->add('method', 'int getSomeProperty()', Annotations::CONTEXT_CLASS);
+        $metadata = new ClassMetadata(ClassMetadata::class, $annotations);
 
         // when serializing and unserializing the metadata
         $unserializedMetadata = unserialize(serialize($metadata));
